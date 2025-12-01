b96ea1859c7d83389c3e58da114ad26dcecf2a5b
jarmandomartinez@correo.unicordoba.edu.co <jarmandomartinez@correo.unicordoba.edu.co>
2025-11-30 23:03:00 -0500

    pom6

--- biblioteca3/src/main/java/proyecto/biblioteca3/config/SwaggerConfig.java
+++ biblioteca3/src/main/java/proyecto/biblioteca3/config/SwaggerConfig.java
@@ -3,15 +3,21 @@ package proyecto.biblioteca3.config;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springdoc.core.models.GroupedOpenApi;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Info apiInfo() {
-        return new Info()
-                .title("API Biblioteca")
-                .version("1.0.0")
-                .description("Sistema de Gestión de Biblioteca");
+    public GroupedOpenApi bibliotecaApi() {
+        return GroupedOpenApi.builder()
+                .group("biblioteca")
+                .addOpenApiCustomizer(openApi -> openApi.info(
+                        new Info()
+                                .title("API Biblioteca")
+                                .version("1.0.0")
+                                .description("Sistema de Gestión de Biblioteca")
+                ))
+                .build();
     }
 }
